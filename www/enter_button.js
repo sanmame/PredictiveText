dfaa9c222bf61095e6eb77b8dd5a24a4b1bcbd43
sanmame <sanmame@gmail.com>
2020-02-19 17:51:11 +0100

    Delete enter_button.js

--- www/enter_button.js
+++
@@ -1,6 +0,0 @@
-$(document).keyup(function(event) {
-    if (($("#query").is(":focus") && (event.key == "Enter")) || 
-    ($("#num_choices").is(":focus") && (event.key == "Enter"))){
-        $("#button").click();
-    }
-});
